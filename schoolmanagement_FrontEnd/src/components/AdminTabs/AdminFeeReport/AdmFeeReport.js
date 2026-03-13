4d5ebaaa84a3dd7b62c34ef67958a0bd7adf9522
Dhruva-D <dhruvad575@gmail.com>
2026-03-13 16:09:46 +0530

    feat: Implement administrative fee report component with filtering, data fetching, and pagination.

--- schoolmanagement_FrontEnd/src/components/AdminTabs/AdminFeeReport/AdmFeeReport.js
+++ schoolmanagement_FrontEnd/src/components/AdminTabs/AdminFeeReport/AdmFeeReport.js
@@ -420,6 +420,7 @@ function AdmFeeReport() {
                       />
                     </div>
 
+                    {/* 
                     <div className="col-12 col-md-4 mb-3">
                       <label className="form-label">Academic Year</label>
                       <Select
@@ -467,7 +468,8 @@ function AdmFeeReport() {
                         isClearable
                         isDisabled={!selectedSemester}
                       />
-                    </div>
+                    </div> 
+                    */}
 
                   </div>
                 </div>
